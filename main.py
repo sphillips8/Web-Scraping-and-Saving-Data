b59523263b0be92ecd55e8e4b2c31d7cc5e14ca3
sphillips8 <sp4gf@virginia.edu>
2022-06-26 03:08:54 +0000

    Saves data to .txt files

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import threading
+from datetime import datetime
+
 
 # User Can Get Xpath by inspecting HTML elements, hover over element, right click, and Copy Full Xpath
 
@@ -17,38 +20,45 @@ def get_driver(url):
   return driver
 
   
-def clean_text(text):
-  ''' Extracts only temperature from text'''
-  output = float(text.split(': ')[1])
+def extract_clean_text(driver, by, value):
+  ''' Returns cleaned element text'''
+  element = driver.find_element(by=by, value=value)
+  output = float(element.text.split(': ')[1])
   return output
 
-  
-def extract_text(url, by, value):
-  ''' Returns element text'''
-  driver = get_driver(url)
-  time.sleep(2) # Waits for average world temperature to load
-  element = driver.find_element(by=by, value=value)
-  return element.text
 
-def log_in(url, username, password):
-  ''' Returns element text'''
-  driver = get_driver(url)
+def log_in(login_url, home_url, username, password):
+  ''' Logs into website and returns to home screen'''
+  driver = get_driver(login_url)
   driver.find_element(by='id', value='id_username').send_keys(username)
   time.sleep(2)
   driver.find_element(by='id', value='id_password').send_keys(password + Keys.RETURN)
   time.sleep(2)
   driver.find_element(by='xpath', value='/html/body/nav/div/a').click()
-  return driver.current_url
+  time.sleep(2)
+  driver = get_driver(home_url)
+  time.sleep(2)
+  return driver
 
-home_url = 'http://automated.pythonanywhere.com'
-login_url = 'http://automated.pythonanywhere.com/login/'
-xpath_h1_1 = '/html/body/div[1]/div/h1[1]'
-xpath_h1_2 = '/html/body/div[1]/div/h1[2]'
 
-# Extracting Text Exercises
-print(extract_text(url=home_url, by='xpath', value=xpath_h1_1))
-print(extract_text(url=home_url, by='xpath', value=xpath_h1_2))
-print('Temperature Only: {}'.format(clean_text(text=extract_text(url=home_url, by='xpath', value=xpath_h1_2))))
+def main():
+  ''' Main function to log in, scrape average temperature, and save data to .txt files'''
+  numIterations = 20
+  pauseSeconds = 5
+  home_url = 'http://automated.pythonanywhere.com'
+  login_url = 'http://automated.pythonanywhere.com/login/'
+  xpath_h1_2 = '/html/body/div[1]/div/h1[2]'
+  # User logs in once
+  driver = log_in(login_url=login_url, home_url=home_url, username='automated', password='automatedautomated')
+  # Saves data to .txt files every 5 seconds. Stops at 20 files
+  i = 0
+  while i <= numIterations:
+    with open('{}.txt'.format(datetime.now()), 'w+') as file:
+      file.write(str(extract_clean_text(driver=driver, by='xpath', value=xpath_h1_2)))
+      file.close()
+    time.sleep(pauseSeconds)
+    i += 1
+  print(' Process Complete')
 
-# Logging In Exercises
-print(log_in(url=login_url, username='automated', password='automatedautomated'))
+
+main()
